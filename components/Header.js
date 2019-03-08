255c263d23c333ecd8d4f851ec2febedd935032c
Simeon <simeoncoding@gmail.com>
2019-03-08 22:03:44 +0000

    add link to new page

--- components/Header.js
+++ components/Header.js
@@ -12,6 +12,9 @@ const Header = () => (
     <Link href="/about">
       <a style={linkStyle}>About</a>
     </Link>
+    <Link href="/sample">
+      <a style={linkStyle}>Sample</a>
+    </Link>
   </div>
 );
 
